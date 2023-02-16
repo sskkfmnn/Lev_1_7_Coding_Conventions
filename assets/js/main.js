88a471b0914cc927394384f9085b2f618976c1b5
sskkfmnn <sskkfmnn@gmail.com>
2023-02-16 14:32:28 +0100

    Lev_1_7

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+console.log("");
+
+let helloWorld = "Hello World";
+console.log (helloWorld);
+
+const pi = 3.14;
+console.log (pi);
+
+let burjDubai = "828m";
+console.log (burjDubai);
+
+let fullName = "Jan Schmidt";
+console.log (fullName);
+
+let eifelTower = "324m";
+console.log (eifelTower);
+
+let camelCase = "camelCase";
+console.log (camelCase);
+
+let kursStatus = true;
+console.log (kursStatus);
